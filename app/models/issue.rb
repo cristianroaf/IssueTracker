38676e9b4cd22e9ac3e162171cc500d1654438aa
Ubuntu <ubuntu@ip-172-31-7-231.us-east-2.compute.internal>
2019-11-14 19:26:48 +0000

    Relaciones en los esquemas

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,8 @@
 class Issue < ApplicationRecord
+    belongs_to :user
+    belongs_to :asignee, class_name: 'User', foreign_key: :asignee_id, optional: true
+    has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
+    has_many :watchers, dependent: :destroy
+    validates :Title, :Type, :Priority, :Status, presence: true
 end
