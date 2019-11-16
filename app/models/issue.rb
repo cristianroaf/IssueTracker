e9fbcf68709521be5e505fcc0c9fcfc903d7dd35
miguel.salamero <miguel.salamero@est.fib.upc.edu>
2019-11-16 11:41:24 +0100

    adds status/type/priority to issue and html new issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,4 +5,14 @@ class Issue < ApplicationRecord
     has_many :votes, dependent: :destroy
     has_many :watchers, dependent: :destroy
     validates :Title, :Type, :Priority, :Status, presence: true
+
+    def self.status
+        ["New", "Open", "On hold", "Resolved", "Duplicate", "Invalid", "Won't fix", "Closed"]
+    end
+    def self.type
+        ["Bug", "Enhancement", "Proposal", "Task"]
+    end
+    def self.priority
+        ["Trivial", "Minor", "Major", "Critical", "Blocker"]
+    end
 end
