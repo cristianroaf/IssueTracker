7180d6d4b4b4ed6d6d9284c04c8175bf66b18914
cristianroaf <roa98@hotmail.com>
2019-12-02 23:39:21 +0000

    api_key working + post to create an issue from api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,78 +1,83 @@
 class ApiController < ApplicationController
+    protect_from_forgery with: :null_session
+    
     
     def issues
         respond_to do |format|
-      @issues = Issue.all
-      
-      if params.has_key?(:assignee)
-        if User.exists?(id: params[:assignee])
-          @issues = @issues.where(asignee_id: params[:assignee])
-        else
-          format.json {render json: {"error":"User with id="+params[:assignee]+" does not exist"}, status: :unprocessable_entity}
-        end
-      end
-      
-      if params.has_key?(:type)
-        @issues = @issues.where(Type: params[:type])
-      end
-      
-      if params.has_key?(:priority)
-        @issues = @issues.where(Priority: params[:priority])
-      end
-      
-      if params.has_key?(:status)
-        if params[:status] == "New&Open"
-          @issues = @issues.where(Status: ["Open","New"])
-        else
-        @issues = @issues.where(Status: params[:status])
-        end
-      end
-      
-      if params.has_key?(:watcher)
-        if User.exists?(id: params[:watcher])
-          @issues = Issue.joins(:watchers).where(watchers:{user_id: params[:watcher]})
-        else
-          format.json {render json: {"error":"User with id="+params[:watcher]+" does not exist"}, status: :unprocessable_entity}
-        end
-      end
-      
-      if params.has_key?(:sort)
-        #if params[:sort] == "votes"
-        # @issues = @issues.order('votes asc')
-        #elsif params[:sort] == "priority"
-        # @issues = @issues.order('priority asc')
-        #elsif params[:sort] == "kind"
-        # @issues = @issues.order('type asc')
-        #elsif params[:sort] == "status"
-        # @issues = @issues.order('status asc')
-        if params[:sort] == "assignee"
-          @issues = @issues.order('asignee_id asc')
-        elsif params[:sort] == "created"
-          @issues = @issues.order('created_at asc')
-        elsif params[:sort] == "updated"
-          @issues = @issues.order('updated_at asc')
-        elsif params[:sort] == "Title"
-          @issues = @issues.order('id asc')
-        #elsif params[:sort] == "-votes"
-        # @issues = @issues.order('votes desc')
-        #elsif params[:sort] == "-priority"
-        # @issues = @issues.order('priority desc')
-        #elsif params[:sort] == "-kind"
-        # @issues = @issues.order('type desc')
-        #elsif params[:sort] == "-status"
-        # @issues = @issues.order('status desc')
-        elsif params[:sort] == "-assignee"
-          @issues = @issues.order('asignee_id desc')
-        elsif params[:sort] == "-created"
-          @issues = @issues.order('created_at desc')
-        elsif params[:sort] == "-updated"
-          @issues = @issues.order('updated_at desc')
-        elsif params[:sort] == "-Title"
-          @issues = @issues.order('id desc')
-        end
-      end
+        @issues = Issue.all
 
-      format.json {render json: @issues, status: :ok}
+        Rails.logger.debug("ENTRA")
+        if params.has_key?(:assignee)
+          if User.exists?(id: params[:assignee])
+            @issues = @issues.where(asignee_id: params[:assignee])
+          else
+            format.json {render json: {"error":"User with id="+params[:assignee]+" does not exist"}, status: :unprocessable_entity}
+          end
+        end
+        
+        if params.has_key?(:type)
+          @issues = @issues.where(Type: params[:type])
+        end
+        
+        if params.has_key?(:priority)
+          @issues = @issues.where(Priority: params[:priority])
+        end
+        
+        if params.has_key?(:status)
+          if params[:status] == "New&Open"
+            @issues = @issues.where(Status: ["Open","New"])
+          else
+          @issues = @issues.where(Status: params[:status])
+          end
+        end
+        
+        if params.has_key?(:watcher)
+          if User.exists?(id: params[:watcher])
+            @issues = Issue.joins(:watchers).where(watchers:{user_id: params[:watcher]})
+          else
+            format.json {render json: {"error":"User with id="+params[:watcher]+" does not exist"}, status: :unprocessable_entity}
+          end
+        end
+  
+        format.json {render json: @issues, status: :ok}
+      end
     end
+    
+    def newissue
+      
+      #------ ESTO SIRVE PARA DEBUGAR
+      #Rails.logger.debug("PARAMS: #{issue_params}")
+      #Rails.logger.debug("API-KEY: #{api_key}")
+      #Rails.logger.debug("USER: #{user.inspect}")
+      #Rails.logger.debug("PARAMS: #{params.inspect}")
+      
+      #HAY QUE BORRAR EL PARAMETRO DE API KEY EN EL MOMENTO QUE SE LEE PORQUE NO ES UN PARAMETRO VALIDO CUANDO SE CREA UNA ISSUE
+      api_key = params[:api_key]
+      params.delete :api_key
+      user = User.where(["authentication_token = ?", api_key]).first
+      @issue = Issue.new(issue_params)
+      @issue.user_id = user.id
+      respond_to do |format|
+        if (issue_params.has_key?(:asignee_id) && issue_params[:asignee_id] != "" && !User.exists?(id: issue_params[:asignee_id]))
+            format.json {render json: {"error":"User with id="+issue_params[:asignee_id]+" does not exist"}, status: :unprocessable_entity}
+        else
+          if @issue.save
+            @watcher = Watcher.new
+            @watcher.user_id = user.id
+            @watcher.issue_id = @issue.id
+            @watcher.save
+            @issue.increment!("Watchers")
+            format.json { render json: @issue, status: :created}
+          else
+            format.json { render json: @issue.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+      
     end
+    
+    def issue_params
+      params.permit(:Title, :Description, :Type, :Priority, :Status, :asignee_id, :user_id, :attachment)
+    end
+    
 end
