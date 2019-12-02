c9e2ab91cb8b953cdbeca047328d3a6f50e092f5
miguel.salamero <miguel.salamero@est.fib.upc.edu>
2019-12-02 20:16:42 +0100

    token approach

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,17 @@ class User < ApplicationRecord
         data = access_token.info
         user = User.where(email: data['email']).first
 
+        dataAuth = access_token.credentials
+
         # Uncomment the section below if you want users to be created if they don't exist
         unless user
             user = User.create(name: data['name'],
             email: data['email'],
-            password: Devise.friendly_token[0,20]
+            password: Devise.friendly_token[0,20],
+            authentication_token: dataAuth['token']
             )
         end
+        puts(user.authentication_token)
         user
     end
 end
